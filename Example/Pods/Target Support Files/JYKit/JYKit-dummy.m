58825e4b1b73c5b2c9fb294e7058559d8997aa25
jiayuwang <jiayuwang@upchina.com>
2024-02-27 16:09:12 +0800

    JYKit组件初始化

---
+++ Example/Pods/Target Support Files/JYKit/JYKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JYKit : NSObject
+@end
+@implementation PodsDummy_JYKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-JYKit_Tests/Pods-JYKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JYKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JYKit_Tests
+@end
